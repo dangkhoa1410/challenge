d350cbacb32c713b520bfadf6565d1a903f6fd8b
KhoaDang <dangkhoa1410@gmail.com>
2021-07-18 15:15:05 -0400

    Fixed API catch errors and added params

--- src/routes/getListOfAgesOfUsersWith.js
+++ src/routes/getListOfAgesOfUsersWith.js
@@ -1,12 +1,16 @@
-'use strict';
-const mockDBCalls = require('../database/index.js');
+"use strict";
+const mockDBCalls = require("../database/index.js");
 
 const getListOfAgesOfUsersWithHandler = async (request, response) => {
-    const itemToLookup = 'carrot';
+  try {
+    const itemToLookup = request.params.lookupItem;
     const data = await mockDBCalls.getListOfAgesOfUsersWith(itemToLookup);
     return response.status(200).send(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = (app) => {
-    app.get('/users/age', getListOfAgesOfUsersWithHandler);
+  app.get("/users/age/:lookupItem", getListOfAgesOfUsersWithHandler);
 };
--- src/routes/getUsers.js
+++ src/routes/getUsers.js
@@ -1,11 +1,15 @@
-'use strict';
-const mockDBCalls = require('../database/index.js');
+"use strict";
+const mockDBCalls = require("../database/index.js");
 
 const getUsersHandler = async (request, response) => {
+  try {
     const data = await mockDBCalls.getUsers();
     return response.status(200).send(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = (app) => {
-    app.get('/users', getUsersHandler);
+  app.get("/users", getUsersHandler);
 };
