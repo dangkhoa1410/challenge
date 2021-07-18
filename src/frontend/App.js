f4e253abd0c6e27ba36d16e3bf68e1b73173f0a5
KhoaDang <dangkhoa1410@gmail.com>
2021-07-18 16:02:21 -0400

    Render All Users

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css";
 import Table from './components/Table';
+import {getUsers} from './components/API'
 
 const App = () => {
+  const [users, setUsers] = useState(null)
+  useEffect (() =>{
+    getUsers()
+    .then(users => setUsers(users))
+  },[])
   
   return (
     <div className="container">
-      <h1>Hello </h1>
+      <div className="row">
+      <h1>All Users </h1>
+      <p>Users and their age</p>
+      <Table thead={['Username','Age']} tcol={['username','age']} tdata={users} />
+      </div>
     </div>
   )
 }
